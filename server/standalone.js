1f9fbb5bd89122cecd52862fbe512c2c4ecaf60a
Nicolas Joyard <nicolas@njoyard.fr>
2018-12-13 18:47:09 +0100

    Add logging to standalone server

--- server/standalone.js
+++ server/standalone.js
@@ -2,7 +2,8 @@
 
 let express = require('express')
 let app = express()
+let morgan = require('morgan')
 
+app.use(morgan('dev'))
 require('./circleci-proxy')(app)
-
 app.listen(Number(process.env.PERFO_PORT) || 4200)
